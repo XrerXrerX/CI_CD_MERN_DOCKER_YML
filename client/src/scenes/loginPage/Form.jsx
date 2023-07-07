6e95a9121c028f82b2b51643b4c7e32774511366
XrerXrerX <50944603+XrerXrerX@users.noreply.github.com>
2023-07-07 11:32:38 +0700

    second commit

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,9 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      // "http://localhost:3001/auth/register",
+      "https://mern-qurious-backend.onrender.com/auth/register",
+
       {
         method: "POST",
         body: formData,
